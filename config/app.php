740a2fee8cbdb3e7a6a420b80ee76ea6a8cdb137
wqz <1280356689@qq.com>
2018-04-24 11:11:01 +0800

    第一次

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Mews\Captcha\CaptchaServiceProvider::class,
+        AliyunOss\Laravel\AliyunOssServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -225,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
+        'OSS' => AliyunOss\Laravel\AliyunOssFacade::class,
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+//商家
+//登录路由
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+Route::get('/help', 'StaticController@help')->name('help');
+Route::get('/name', 'StaticController@show')->name('show');
+/*Route::resource('buys','BuysController');*/
+Route::resource('admin','AdminController');
+Route::resource('plople','PlopleController');
+Route::get('/password', 'PasswordController@index')->name('password');
+Route::post('/password', 'PasswordController@edit')->name('edit');
+Route::resource('goods','GoodsController');
+Route::resource('goodscate','GoodscateController');
+Route::post('/upload', 'UploaderController@upload');
+
+Route::resource('activity','ActivityController');
